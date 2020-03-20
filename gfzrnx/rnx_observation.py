6506af0cd211ac0d483b3ecade2c8f99421cfea6
alainmuls <alain.muls@gmail.com>
2020-03-20 15:40:32 +0100

    created the 'analysed' key which identifies what is going to be analysed

--- gfzrnx/rnx_observation.py
+++ gfzrnx/rnx_observation.py
@@ -5,17 +5,17 @@ from termcolor import colored
 import json
 
 import am_config as amc
-from ampyutils import  exeprogram
+from ampyutils import  exeprogram, amutils
 
 
-def rnxobs_header_metadata(dRnx: dict, dProgs:dict, logger: logging.Logger) -> dict:
+def rnxobs_header_metadata(dArgs: dict, dProgs:dict, logger: logging.Logger) -> dict:
     """
     rnxobs_header_metadata reads the rinex observation file header and extracts info
     """
     cFuncName = colored(os.path.basename(__file__), 'yellow') + ' - ' + colored(sys._getframe().f_code.co_name, 'green')
 
     # extract the header meta data into a json structure
-    cmdGFZRNX = '{prog:s} -meta basic:jsonp -finp {obs:s} -fout /tmp/{obs:s}.json'.format(prog=dProgs['gfzrnx'], obs=dRnx['obs_name'])
+    cmdGFZRNX = '{prog:s} -meta basic:jsonp -finp {obs:s} -fout /tmp/{obs:s}.json'.format(prog=dProgs['gfzrnx'], obs=dArgs['obs_name'])
     print('cmdGFZRNX = {!s}'.format(cmdGFZRNX))
     logger.info('{func:s}: Running:\n{cmd:s}'.format(func=cFuncName, cmd=colored(cmdGFZRNX, 'blue')))
 
@@ -23,14 +23,53 @@ def rnxobs_header_metadata(dRnx: dict, dProgs:dict, logger: logging.Logger) -> d
     # gfzrnx -finp data/P1710171.20O -meta basic:jsonp
     exeprogram.subProcessDisplayStdErr(cmd=cmdGFZRNX, verbose=False)
 
-    with open('/tmp/{obs:s}.json'.format(obs=dRnx['obs_name'])) as json_file:
+    with open('/tmp/{obs:s}.json'.format(obs=dArgs['obs_name'])) as json_file:
         hdr_obs = json.load(json_file)
 
     return hdr_obs
 
 
-def rnxobs_metadata_parser(dObsHdr: dict, logger: logging.Logger):
+def rnxobs_metadata_parser(dobs_hdr: dict, dArgs: dict, logger: logging.Logger) -> dict:
     """
     rnxobs_metadata_parser parser useful info from the header and stores in dictionary
     """
     cFuncName = colored(os.path.basename(__file__), 'yellow') + ' - ' + colored(sys._getframe().f_code.co_name, 'green')
+
+    # dictionary containing relevant info
+    drnx_obs = {}
+
+    # get the GNSSs cavailable
+    rnx_info = []
+    GNSSs = list(dobs_hdr['file']['satsys'])
+    for gnss in GNSSs:
+        # create info text to inform user what is available
+        txt_info = '\n   GNSS: {gnss:s}'.format(gnss=gnss)
+        txt_info += '\n      frequencies: {freq!s}'.format(gnss=gnss, freq=', '.join(dobs_hdr['file']['sysfrq'][gnss]))
+        txt_info += '\n      observation types: {type!s}'.format(type=', '.join(dobs_hdr['file']['systyp'][gnss]))
+        txt_info += '\n      observation codes: {obs!s}'.format(obs=', '.join(dobs_hdr['file']['sysobs'][gnss]))
+        logger.info('{func:s}: Available {info:s}'.format(info=txt_info, func=cFuncName))
+
+        if gnss in dArgs['gnss']:
+            drnx_obs[gnss] = {}
+
+            # get the frequencies for this GNSS
+            drnx_obs[gnss]['sysfrq'] = [freq for freq in dobs_hdr['file']['sysfrq'][gnss] if freq in dArgs['sysfrq']]
+            # get the observation types for this GNSS
+            drnx_obs[gnss]['systyp'] = [systyp for systyp in dobs_hdr['file']['systyp'][gnss] if systyp in dArgs['systyp']]
+
+            # get the observation codes for this GNSS
+            drnx_obs[gnss]['sysobs'] = [sysobs for sysobs in dobs_hdr['file']['sysobs'][gnss] if sysobs[0] in dArgs['systyp']]
+
+            # create list of possible PRNs for this GNSS
+            print(dArgs['prns'])
+            print(type(dArgs['prns']))
+            drnx_obs[gnss]['prns'] = ['{gnss:s}{prn:02d}'.format(gnss=gnss, prn=prn) for prn in range(1, 37) if prn in dArgs['prns'] ]
+
+            # create info text to inform user what is available
+            txt_exam  = '\n   GNSS: {gnss:s}'.format(gnss=gnss)
+            txt_exam += '\n      frequencies: {freq!s}'.format(freq=', '.join(drnx_obs[gnss]['sysfrq']))
+            txt_exam += '\n      observation codes: {obs!s}'.format(obs=', '.join(drnx_obs[gnss]['sysobs']))
+            txt_exam += '\n      PRNs: {prns!s}'.format(prns=', '.join(drnx_obs[gnss]['prns']))
+            logger.info('{func:s}: Analysed {info:s}'.format(info=txt_exam, func=cFuncName))
+
+    return drnx_obs
--- pygfzrnx.py
+++ pygfzrnx.py
@@ -17,9 +17,19 @@ __author__ = 'amuls'
 class prn_action(argparse.Action):
     def __call__(self, parser, namespace, PRNs, option_string=None):
         for prn in PRNs:
-            if not 0 < int(prn) < 37 or prn == 'all':
-                raise argparse.ArgumentError(self, "PRNs must be in 1..36")
-        setattr(namespace, self.dest, PRNs)
+            print('prn = {!s}'.format(prn))
+            try:
+                if not 0 < int(prn) < 37:
+                    raise argparse.ArgumentError(self, "PRNs must be in 1..36 or 'all'")
+            except ValueError:
+                if prn != 'all':
+                    raise argparse.ArgumentError(self, "PRNs must be in 1..36 or 'all'")
+        if prn == 'all':
+            ret_prns = [prn for prn in range(1, 37)]
+            setattr(namespace, self.dest, ret_prns)
+        else:
+            ret_prns = [int(iprn) for iprn in PRNs]
+            setattr(namespace, self.dest, ret_prns)
 
 
 class logging_action(argparse.Action):
@@ -31,6 +41,15 @@ class logging_action(argparse.Action):
             if log_action not in choices:
                 raise argparse.ArgumentError(self, "log_actions must be in {!s}".format(choices))
         setattr(namespace, self.dest, log_actions)
+
+
+class freq_action(argparse.Action):
+    def __call__(self, parser, namespace, freqs, option_string=None):
+        for freq in freqs:
+            print('freq = {!s}'.format(freq))
+            if freq not in ['1', '2', '5', '6'] and freq != 'all':
+                raise argparse.ArgumentError(self, "freqs must be either 1, 2, 5, 6 or 'all'")
+        setattr(namespace, self.dest, freqs)
 
 
 def treatCmdOpts(argv):
@@ -50,7 +69,8 @@ def treatCmdOpts(argv):
 
     parser.add_argument('-o', '--obs_type', help='select observation types to plot (default {:s})'.format(colored('C', 'green')), default='C', choices=['C', 'S', 'D', 'L'], nargs='+', type=str)
     parser.add_argument('-s', '--sat_syst', help='select GNNSs (default to {:s})'.format(colored('E', 'green')), default='E', choices=['E', 'G'], nargs='+', type=str)
-    parser.add_argument('-p', '--prn', help='select PRNs (default {:s})'.format(colored('all', 'green')), action=prn_action, nargs='+')
+    parser.add_argument('-f', '--freqs', help='select frequency bands (default to {:s})'.format(colored('all', 'green')), default='all', nargs='+', action=freq_action)
+    parser.add_argument('-p', '--prn', help='select PRNs (default {:s})'.format(colored('all', 'green')), default='all', action=prn_action, nargs='+')
 
 
     parser.add_argument('-l', '--logging', help='specify logging level console/file (default {:s})'.format(colored('INFO DEBUG', 'green')), nargs=2, required=False, default=['INFO', 'DEBUG'], action=logging_action)
@@ -59,7 +79,7 @@ def treatCmdOpts(argv):
 
     args = parser.parse_args()
 
-    return args.obsRnx, args.dirRnx, args.obs_type, args.sat_syst, args.prn, args.logging
+    return args.obsRnx, args.dirRnx, args.obs_type, args.sat_syst, args.freqs, args.prn, args.logging
 
 
 def checkArguments(logger: logging.Logger):
@@ -70,8 +90,8 @@ def checkArguments(logger: logging.Logger):
 
     # change to the directory dirRnx if it exists
     workDir = os.getcwd()
-    if amc.dRTK['rinex']['dir'] != '.':
-        workDir = os.path.normpath(os.path.join(workDir, amc.dRTK['rinex']['dir']))
+    if amc.dRTK['args']['dir'] != '.':
+        workDir = os.path.normpath(os.path.join(workDir, amc.dRTK['args']['dir']))
     logger.info('{func:s}: working directory is {dir:s}'.format(func=cFuncName, dir=colored('{:s}'.format(workDir), 'green')))
 
     if not os.path.exists(workDir):
@@ -82,10 +102,10 @@ def checkArguments(logger: logging.Logger):
         logger.info('{func:s}: changed to directory {dir:s}'.format(func=cFuncName, dir=colored('{:s}'.format(workDir), 'green')))
 
     # check existence of RINEX observation file
-    if not os.access(amc.dRTK['rinex']['obs_name'], os.R_OK):
-        logger.error('{func:s}: RINEX observation file {rinex:s} not accessible.\n'.format(func=cFuncName, rinex=colored('{!s}'.format(amc.dRTK['rinex']['obs_name']), 'red')))
+    if not os.access(amc.dRTK['args']['obs_name'], os.R_OK):
+        logger.error('{func:s}: RINEX observation file {rinex:s} not accessible.\n'.format(func=cFuncName, rinex=colored('{!s}'.format(amc.dRTK['args']['obs_name']), 'red')))
         return amc.E_FILE_NOT_EXIST
-    logger.info('{func:s}: RINEX observation file {obs:s} accessible'.format(func=cFuncName, obs=colored('{!s}'.format(amc.dRTK['rinex']['obs_name']), 'green')))
+    logger.info('{func:s}: RINEX observation file {obs:s} accessible'.format(func=cFuncName, obs=colored('{!s}'.format(amc.dRTK['args']['obs_name']), 'green')))
 
 
 def main(argv):
@@ -96,21 +116,22 @@ def main(argv):
     cFuncName = colored(os.path.basename(__file__), 'yellow') + ' - ' + colored(sys._getframe().f_code.co_name, 'green')
 
     # treat command line options
-    obsRnx, dirRnx, obs_types, sat_systs, prns, logLevels = treatCmdOpts(argv)
+    obsRnx, dirRnx, obs_types, sat_systs, frequencies, prns, logLevels = treatCmdOpts(argv)
 
     # store cli parameters
     amc.dRTK = {}
-    dObs = {}
-    amc.dRTK['obs'] = dObs
+    dInfo = {}
+    amc.dRTK['info'] = dInfo
 
-    dRnx = {}
-    dRnx['dir'] = dirRnx
-    dRnx['obs_name'] = obsRnx
-    dRnx['obs_types'] = obs_types
-    dRnx['sat_systs'] = sat_systs
-    dRnx['prns'] = prns
+    dArgs = {}
+    dArgs['dir'] = dirRnx
+    dArgs['obs_name'] = obsRnx
+    dArgs['gnss'] = sat_systs
+    dArgs['systyp'] = obs_types
+    dArgs['sysfrq'] = frequencies
+    dArgs['prns'] = prns
 
-    amc.dRTK['rinex'] = dRnx
+    amc.dRTK['args'] = dArgs
 
     # create logging for better debugging
     logger = amc.createLoggers(os.path.basename(__file__), dir=dirRnx, logLevels=logLevels)
@@ -123,9 +144,9 @@ def main(argv):
     checkArguments(logger=logger)
 
     # read the header info using gfzrnx
-    amc.dRTK['obs']['header'] = rnxobs.rnxobs_header_metadata(dRnx=amc.dRTK['rinex'], dProgs=amc.dRTK['progs'], logger=logger)
+    amc.dRTK['info']['header'] = rnxobs.rnxobs_header_metadata(dArgs=amc.dRTK['args'], dProgs=amc.dRTK['progs'], logger=logger)
     # extract parts of the rinex observation header
-    rnxobs.rnxobs_metadata_parser(dObsHdr=amc.dRTK['obs']['header'], logger=logger)
+    amc.dRTK['analysed'] = rnxobs.rnxobs_metadata_parser(dobs_hdr=amc.dRTK['info']['header'], dArgs=amc.dRTK['args'], logger=logger)
 
     # show the information JSON structure
     logger.info('{func:s}: info dictionary = \n{prt!s}'.format(prt=amutils.pretty(amc.dRTK), func=cFuncName))
